7b8a92656f3c7b409d5d4d002e67610229eecdcc
Casey Hofland <hofland.casey@gmail.com>
2019-12-08 21:13:51 +0100

    Added Vuforia Common Samples from the Unity Asset Store

--- HybridSpace/Assets/Public Displayer/Controllers.cs
+++ HybridSpace/Assets/Public Displayer/Controllers.cs
@@ -3,89 +3,139 @@ namespace UnityEngine
 {
 	public static class Controllers
 	{
-		public static class DialogueController
+		public static class Astronaut
 		{
 			public static class Parameters
 			{
-				public const int Next = -1541554430;
-				public const int Stop = 426031496;
+				public const int DoEnter = 1780034540;
+				public const int IsWaving = 441653142;
+				public const int IsDrilling = -213092351;
 			}
 		
 			public static class Tags
 			{
 			}
 		
-			public static class BaseLayer
+			public static class astronaut
 			{
 				public const int index = 0;
 		
-				public const int NewState = -499022955;
+				public const int astronautidle = 76268130;
+				public const int astronautdrilling = 567435778;
+				public const int astronautidletodrilling = -596221306;
+				public const int astronautwave = 44817150;
 			}
 		}
 		
-		public static class Controller1
+		public static class Drone
 		{
 			public static class Parameters
 			{
-				public const int Float = 140800401;
-				public const int Float0 = -1109592988;
-				public const int Float1 = -891566862;
-				public const int NewFloat = -2005463045;
+				public const int DoEnter = 1780034540;
+				public const int IsFacingObject = 684555715;
+				public const int IsScanning = 2013384399;
+				public const int IsShowingLaser = -1010458557;
 			}
 		
 			public static class Tags
 			{
-				public const string SomeTag = @"Some Tag";
 			}
 		
-			public static class BaseLayer
+			public static class drone
 			{
 				public const int index = 0;
 		
-				public const int NewState = -499022955;
-				public const int NewState0 = -1979496331;
-				public const int NewState1 = -50038557;
+				public const int droneidle = -1415987755;
+				public const int droneenter = 576962518;
 		
-				public static class _BaseLayer
+				public static class dronescan
 				{
-					public const int NewState = -499022955;
-					public const int NewState0 = -1979496331;
-		
-					public static class NewStateMachine
-					{
-						public const int NewState = -499022955;
-					}
-				}
-		
-				public static class NewState10
-				{
-					public const int NewState = -499022955;
+					public const int dronefaceObject = 1920794402;
+					public const int _dronescan = -1290880146;
 				}
 			}
 		
-			public static class BaseLayer0
+			public static class scan
 			{
 				public const int index = 1;
 		
-				public const int NewState = -499022955;
-			}
-		
-			public static class BaseLayer00
-			{
-				public const int index = 2;
-			}
-		
-			public static class Tags0
-			{
-				public const int index = 3;
+				public const int dronescanoff = 2046325463;
+				public const int dronescanon = 628853627;
+				public const int dronescanontooff = -1115408957;
+				public const int dronescanofftoon = -1983509958;
 			}
 		}
 		
-		public static class NewAnimatorController
+		public static class Habitat
 		{
 			public static class Parameters
 			{
-				public const int NewBool = -2085996487;
+				public const int IsDoorOpen = -946555215;
+				public const int IsHatchOpen = -1191907359;
+			}
+		
+			public static class Tags
+			{
+			}
+		
+			public static class door
+			{
+				public const int index = 0;
+		
+				public const int habdoorclosed = 363235099;
+				public const int habdoorclosedtoopen = 1368671572;
+				public const int habdooropen = -715289285;
+				public const int habdooropentoclosed = -703389471;
+			}
+		
+			public static class hatch
+			{
+				public const int index = 1;
+		
+				public const int habhatchclosed = -102472959;
+				public const int habhatchclosedtoopen = -457938166;
+				public const int habhatchopen = 2006317327;
+				public const int habhatchopentoclosed = 1664290495;
+			}
+		}
+		
+		public static class OxygenTank
+		{
+			public static class Parameters
+			{
+				public const int DoEnter = 1780034540;
+				public const int IsDetailOn = -759459797;
+			}
+		
+			public static class Tags
+			{
+			}
+		
+			public static class oxygenTank
+			{
+				public const int index = 0;
+		
+				public const int oxygenTankenter = 1685633342;
+				public const int oxygenTankidle = -652137350;
+			}
+		
+			public static class detail
+			{
+				public const int index = 1;
+		
+				public const int oxygenTankdetailoff = -913035785;
+				public const int oxygenTankdetailofftoon = -2138578499;
+				public const int oxygenTankdetailon = -1715697407;
+				public const int oxygenTankdetailontooff = -1262100924;
+			}
+		}
+		
+		public static class Panel
+		{
+			public static class Parameters
+			{
+				public const int ShowPanel = 1615845488;
+				public const int HidePanel = -831917272;
 			}
 		
 			public static class Tags
@@ -95,6 +145,10 @@ namespace UnityEngine
 			public static class BaseLayer
 			{
 				public const int index = 0;
+		
+				public const int ShowAnim = 1382005991;
+				public const int HideAnim = -1524716017;
+				public const int IdleAnim = -466596548;
 			}
 		}
 	}
