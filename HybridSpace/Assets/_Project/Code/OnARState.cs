74ec25c3278cfeb9089749bc169bfbd947d4b471
Fvanbrummelen <darthfebo@users.noreply.github.com>
2019-12-15 13:16:19 +0100

    Added an eye that is always looking at the camera

---
+++ HybridSpace/Assets/_Project/Code/OnARState.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+[RequireComponent(typeof(ImageTargetBehaviour))]
+public class OnARState : MonoBehaviour
+{
+    [SerializeField]
+    private TrackableBehaviour.Status state = TrackableBehaviour.Status.TRACKED;
+
+    [SerializeField]
+    private UnityEvent onEnterState = new UnityEvent();
+    [SerializeField]
+    private UnityEvent onLeaveState = new UnityEvent();
+
+    private ImageTargetBehaviour target;
+    private bool entered = false;
+
+    private void Start()
+    {
+        target = GetComponent<ImageTargetBehaviour>();
+    }
+
+    private void Update()
+    {
+        if(!entered)
+        {
+            if (target.CurrentStatus == state)
+            {
+                onEnterState.Invoke();
+                entered = true;
+            }
+        }
+        else
+        {
+            if (target.CurrentStatus != state)
+            {
+                onLeaveState.Invoke();
+                entered = false;
+            }
+        }
+    }
+}
