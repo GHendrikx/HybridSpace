e2a59f33d4668c5e465c3588d7683fd24bf8d471
Geoffrey86 <geoffreyhendrikx@hotmail.com>
2019-12-15 18:56:24 +0100

    Added the item.cs and the SoundManager. the prefab for the canvas also

---
+++ HybridSpace/Assets/Script/Item.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private string itemName;
+    [SerializeField]
+    private string itemDescription;
+    [SerializeField]
+    private Text itemText;
+    [SerializeField]
+    private Text description;
+    private bool scanned;
+    
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        //itemText.text = itemName;
+        //description.text = itemDescription;
+        scanned = true;
+        Debug.Log("Enabled");
+    }
+}
---
+++ HybridSpace/Assets/Script/Manager/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    private AudioSource audioSource;
+    private AudioClip[] clips;
+    private bool clipIsPlayed;
+    private void Start()
+    {
+        audioSource = Camera.main.GetComponent<AudioSource>();
+        Resources.LoadAll<AudioClip>("Clips/");
+    }
+
+    public void PlaySound(int index)
+    {
+        if (clipIsPlayed)
+            return;
+        audioSource.clip = clips[index];
+        audioSource.Play();
+        clipIsPlayed = true;
+        StartCoroutine(WaitTillSoundIsPlayed(clips[index].length));
+
+    }
+
+    private IEnumerator WaitTillSoundIsPlayed(float time)
+    {
+        yield return new WaitForSeconds(time);
+        clipIsPlayed = false;
+    }
+}
