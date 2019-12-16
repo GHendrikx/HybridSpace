c74c64e792c692678674842ef45df0cbb37eb610
Geoffrey86 <geoffreyhendrikx@hotmail.com>
2019-12-16 18:37:07 +0100

    Pushing the ARManager

---
+++ HybridSpace/Assets/Script/Manager/ARManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ARManager : MonoBehaviour
+{
+    [SerializeField]
+    private Items[] items;
+}
+
+[System.Serializable]
+public struct Items
+{
+    [SerializeField]
+    private ARItems itemType;
+    [SerializeField]
+    private Item item;
+}
+
+public enum ARItems
+{
+    Vinyl_record,
+    paperweight,
+    TV,
+    Music_box,
+    Bible,
+    Book,
+    UV_lamp,
+    Lamp,
+    Vinyl_player,
+    Water_glass,
+    Family_photo,
+    Cake_mold,
+    Knife,
+    Moving_box,
+    Polaroid_camera,
+}
